c46ed8adfc00a6dc727ecc6fe523f3a86fa15399
Mehdi Ben Hadj Ali <mehdibenhadjali@gmail.com>
2020-11-04 03:34:40 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {Form, Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <article className="App-article">
+        <Form className="formulaire">
+        <h1 className="Title">Formulaire Gomycode</h1>
+          <Form.Row>
+            <Form.Label>Nom</Form.Label>
+            <Form.Control placeholder="Foulen"/>
+          </Form.Row>
+          <Form.Row>
+            <Form.Label>Prénom</Form.Label>
+            <Form.Control placeholder="Ben Foulen"/>
+          </Form.Row>
+          <Form.Row>
+            <Form.Label>Date de naissance</Form.Label>
+            <Form.Control type="datetime-local"/>
+          </Form.Row>
+          <Form.Row>
+            <Form.Label>Adresse Email</Form.Label>
+            <Form.Control placeholder="exemple@gmail.com"/>
+          </Form.Row>
+          <Form.Row>
+            <Form.Label>Profession</Form.Label>
+            <Form.Control placeholder="Ingénieur, médecin, avocat..."/>
+          </Form.Row>
+        <Button style={{marginTop:"20px"}}>Envoyer</Button>
+        </Form>
+      </article>
     </div>
   );
 }
